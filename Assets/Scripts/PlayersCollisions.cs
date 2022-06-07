eb749637d46ceae6a338ec152fd3fe05b8ae92dd
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-06-07 10:41:49 -0300

    change color finished

---
+++ Assets/Scripts/PlayersCollisions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player2")
+		{ 
+			GetComponent<MeshRenderer>().material.color = Color.red;
+		}
+		if (col.gameObject.tag == "Player1")
+		{
+			GetComponent<MeshRenderer>().material.color = Color.blue;
+		}
+	}
+}
