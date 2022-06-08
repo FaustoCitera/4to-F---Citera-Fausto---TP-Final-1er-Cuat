cacb1b66e733ef4aa205f5ef585c8d1d1840a454
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-06-08 11:56:49 -0300

    reloj terminado

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -5,17 +5,9 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-	public Text txt_time;
-	public Text txt_timeFloored;
 	public Text txt_min;
 	public Text txt_segs;
-
-	public GameObject txtPerdiste;
-
 	public float elapsedTime;
-	public float timeToDoSomething;
-	public float timeToWait;
-
 	public bool isCounting;
 
 	// Start is called before the first frame update
@@ -29,30 +21,13 @@ public class TimeManager : MonoBehaviour
 	void Update()
 	{
 		float currentTime = Time.time;
-
-		txt_time.text = currentTime.ToString();
-
-		txt_timeFloored.text = Mathf.Floor(currentTime).ToString();
-
 		int minutos = Mathf.FloorToInt(((currentTime) / 60));
 		txt_min.text = minutos.ToString();
-
 		txt_segs.text = Mathf.Floor(currentTime % 60).ToString();
-
 		if (isCounting)
 		{
 			elapsedTime += Time.deltaTime;
 		}
-
-		if (currentTime > 3)
-		{
-			txtPerdiste.SetActive(true);
-		}
-
-		if (currentTime > timeToDoSomething)
-		{
-			timeToDoSomething += timeToWait;
-			txtPerdiste.GetComponent<Text>().text = "Cambiaré en " + timeToDoSomething.ToString();
-		}
+		
 	}
 }
