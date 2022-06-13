68ce61dc46623d22ec589243b40e73e02b49e93b
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-06-13 10:38:00 -0300

    mil cambios

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
+	public TextMeshPro TMP_Min;
+	public TextMeshPro TMP_seg;
 	public Text txt_min;
 	public Text txt_segs;
 	public float elapsedTime;
 	public bool isCounting;
-
+	float currentTime = 0;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -21,10 +24,12 @@ public class TimeManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		float currentTime = Time.time;
+		currentTime += Time.deltaTime;
 		int minutos = Mathf.FloorToInt(((currentTime) / 60));
-		txt_min.text = minutos.ToString();
-		txt_segs.text = Mathf.Floor(currentTime % 60).ToString();
+		//txt_min.text = minutos.ToString();
+		TMP_Min.text = minutos.ToString();
+		//txt_segs.text = Mathf.Floor(currentTime % 60).ToString();
+		TMP_seg.text = Mathf.Floor(currentTime % 60).ToString();
 		if (isCounting)
 		{
 			elapsedTime += Time.deltaTime;
